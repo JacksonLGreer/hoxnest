6502ab13aa85ff0a8e7875805384ca67b8983b42
JacksonLGreer <jlg40585@uga.edu>
2024-12-16 16:18:56 -0500

    Created header and removed unwanted TypeScript stuff on page

---
+++ hoxnest/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import react from "react";
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <div className={styles.headerDiv}>
+            <h1>HoxNest</h1>
+        </div>
+    )
+}
